068155da548efa0c22a07d4454f9688b60a821c4
ananta23-droid <anantacsit23@oic.edu.np>
2026-09-10 14:54:25 +0545

    Add book search implementation

--- LibraryManagementSystem/Forms/SearchBookForm.Designer.cs
+++ LibraryManagementSystem/Forms/SearchBookForm.Designer.cs
@@ -6,6 +6,13 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lblSearchBy;
+        private System.Windows.Forms.ComboBox cmbSearchBy;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgvSearchResults;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -29,9 +36,103 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lblSearchBy = new System.Windows.Forms.Label();
+            this.cmbSearchBy = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvSearchResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbTitle.Location = new System.Drawing.Point(16, 16);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(140, 32);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Search Books";
+            // 
+            // lblSearchBy
+            // 
+            this.lblSearchBy.AutoSize = true;
+            this.lblSearchBy.Location = new System.Drawing.Point(16, 64);
+            this.lblSearchBy.Name = "lblSearchBy";
+            this.lblSearchBy.Size = new System.Drawing.Size(70, 20);
+            this.lblSearchBy.TabIndex = 1;
+            this.lblSearchBy.Text = "Search By";
+            // 
+            // cmbSearchBy
+            // 
+            this.cmbSearchBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSearchBy.FormattingEnabled = true;
+            this.cmbSearchBy.Location = new System.Drawing.Point(16, 87);
+            this.cmbSearchBy.Name = "cmbSearchBy";
+            this.cmbSearchBy.Size = new System.Drawing.Size(200, 28);
+            this.cmbSearchBy.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(232, 87);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(320, 27);
+            this.txtSearch.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(568, 84);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(88, 34);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(672, 84);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 34);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dgvSearchResults
+            // 
+            this.dgvSearchResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSearchResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSearchResults.Location = new System.Drawing.Point(16, 140);
+            this.dgvSearchResults.MultiSelect = false;
+            this.dgvSearchResults.Name = "dgvSearchResults";
+            this.dgvSearchResults.ReadOnly = true;
+            this.dgvSearchResults.RowHeadersWidth = 51;
+            this.dgvSearchResults.RowTemplate.Height = 29;
+            this.dgvSearchResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSearchResults.Size = new System.Drawing.Size(960, 360);
+            this.dgvSearchResults.TabIndex = 6;
+            // 
+            // SearchBookForm
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SearchBookForm";
+            this.ClientSize = new System.Drawing.Size(1000, 520);
+            this.Controls.Add(this.dgvSearchResults);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cmbSearchBy);
+            this.Controls.Add(this.lblSearchBy);
+            this.Controls.Add(this.lbTitle);
+            this.Name = "SearchBookForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search Books";
+            this.Load += new System.EventHandler(this.SearchBookForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
