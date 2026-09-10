12286abee4ccd596219e15237a72339b8839c816
imArpanGhimire <arpancalma@gmail.com>
2026-09-10 23:14:21 +0545

    Add member and book issue implementation

--- LibraryManagementSystem/Forms/IssueBookForm.Designer.cs
+++ LibraryManagementSystem/Forms/IssueBookForm.Designer.cs
@@ -29,10 +29,171 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lblBook = new System.Windows.Forms.Label();
+            this.lblMember = new System.Windows.Forms.Label();
+            this.lblIssueDate = new System.Windows.Forms.Label();
+            this.lblDueDate = new System.Windows.Forms.Label();
+            this.cmbBook = new System.Windows.Forms.ComboBox();
+            this.cmbMember = new System.Windows.Forms.ComboBox();
+            this.dtpIssueDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.btnIssue = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvIssues = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIssues)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbTitle.Location = new System.Drawing.Point(16, 16);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(124, 25);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Issue Book";
+            // 
+            // lblBook
+            // 
+            this.lblBook.AutoSize = true;
+            this.lblBook.Location = new System.Drawing.Point(16, 64);
+            this.lblBook.Name = "lblBook";
+            this.lblBook.Size = new System.Drawing.Size(39, 15);
+            this.lblBook.TabIndex = 1;
+            this.lblBook.Text = "Book";
+            // 
+            // cmbBook
+            // 
+            this.cmbBook.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBook.FormattingEnabled = true;
+            this.cmbBook.Location = new System.Drawing.Point(16, 87);
+            this.cmbBook.Name = "cmbBook";
+            this.cmbBook.Size = new System.Drawing.Size(360, 23);
+            this.cmbBook.TabIndex = 2;
+            // 
+            // lblMember
+            // 
+            this.lblMember.AutoSize = true;
+            this.lblMember.Location = new System.Drawing.Point(16, 128);
+            this.lblMember.Name = "lblMember";
+            this.lblMember.Size = new System.Drawing.Size(55, 15);
+            this.lblMember.TabIndex = 3;
+            this.lblMember.Text = "Member";
+            // 
+            // cmbMember
+            // 
+            this.cmbMember.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMember.FormattingEnabled = true;
+            this.cmbMember.Location = new System.Drawing.Point(16, 151);
+            this.cmbMember.Name = "cmbMember";
+            this.cmbMember.Size = new System.Drawing.Size(360, 23);
+            this.cmbMember.TabIndex = 4;
+            // 
+            // lblIssueDate
+            // 
+            this.lblIssueDate.AutoSize = true;
+            this.lblIssueDate.Location = new System.Drawing.Point(16, 192);
+            this.lblIssueDate.Name = "lblIssueDate";
+            this.lblIssueDate.Size = new System.Drawing.Size(61, 15);
+            this.lblIssueDate.TabIndex = 5;
+            this.lblIssueDate.Text = "Issue Date";
+            // 
+            // dtpIssueDate
+            // 
+            this.dtpIssueDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpIssueDate.Location = new System.Drawing.Point(16, 215);
+            this.dtpIssueDate.Name = "dtpIssueDate";
+            this.dtpIssueDate.Size = new System.Drawing.Size(160, 23);
+            this.dtpIssueDate.TabIndex = 6;
+            // 
+            // lblDueDate
+            // 
+            this.lblDueDate.AutoSize = true;
+            this.lblDueDate.Location = new System.Drawing.Point(216, 192);
+            this.lblDueDate.Name = "lblDueDate";
+            this.lblDueDate.Size = new System.Drawing.Size(56, 15);
+            this.lblDueDate.TabIndex = 7;
+            this.lblDueDate.Text = "Due Date";
+            // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDueDate.Location = new System.Drawing.Point(216, 215);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(160, 23);
+            this.dtpDueDate.TabIndex = 8;
+            // 
+            // btnIssue
+            // 
+            this.btnIssue.Location = new System.Drawing.Point(16, 260);
+            this.btnIssue.Name = "btnIssue";
+            this.btnIssue.Size = new System.Drawing.Size(88, 34);
+            this.btnIssue.TabIndex = 9;
+            this.btnIssue.Text = "Issue";
+            this.btnIssue.UseVisualStyleBackColor = true;
+            this.btnIssue.Click += new System.EventHandler(this.btnIssue_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(120, 260);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 34);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dgvIssues
+            // 
+            this.dgvIssues.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvIssues.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIssues.Location = new System.Drawing.Point(400, 16);
+            this.dgvIssues.MultiSelect = false;
+            this.dgvIssues.Name = "dgvIssues";
+            this.dgvIssues.ReadOnly = true;
+            this.dgvIssues.RowTemplate.Height = 25;
+            this.dgvIssues.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvIssues.Size = new System.Drawing.Size(560, 450);
+            this.dgvIssues.TabIndex = 11;
+            // 
+            // IssueBookForm
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "IssueBookForm";
+            this.ClientSize = new System.Drawing.Size(980, 500);
+            this.Controls.Add(this.dgvIssues);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnIssue);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.lblDueDate);
+            this.Controls.Add(this.dtpIssueDate);
+            this.Controls.Add(this.lblIssueDate);
+            this.Controls.Add(this.cmbMember);
+            this.Controls.Add(this.lblMember);
+            this.Controls.Add(this.cmbBook);
+            this.Controls.Add(this.lblBook);
+            this.Controls.Add(this.lbTitle);
+            this.Name = "IssueBookForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Issue Book";
+            this.Load += new System.EventHandler(this.IssueBookForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIssues)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lblBook;
+        private System.Windows.Forms.Label lblMember;
+        private System.Windows.Forms.Label lblIssueDate;
+        private System.Windows.Forms.Label lblDueDate;
+        private System.Windows.Forms.ComboBox cmbBook;
+        private System.Windows.Forms.ComboBox cmbMember;
+        private System.Windows.Forms.DateTimePicker dtpIssueDate;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.Button btnIssue;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgvIssues;
 
         #endregion
     }
